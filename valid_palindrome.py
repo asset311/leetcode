0cde55a8e20c8596bf7a81c93382bd796cb9b602
asset311 <32253546+Aseka311@users.noreply.github.com>
2020-06-09 18:42:12 -0400

    valid palindrome

---
+++ valid_palindrome.py
@@ -0,0 +1,33 @@
+'''
+125. Valid Palindrome
+Given a string, determine if it is a palindrome, considering only alphanumeric characters and ignoring cases.
+
+Note: 
+For the purpose of this problem, we define empty string as valid palindrome.
+
+Example 1:
+Input: "A man, a plan, a canal: Panama"
+Output: true
+
+Example 2:
+Input: "race a car"
+Output: false
+'''
+
+# We can solve this problem in O(n) time by using two pointers
+# Pointers will move in opposite directions, and advance on non-alphanumeric
+# If at any point letters aren't equal, it's not a palindrome
+
+def isPalindrome(s:str) -> bool:
+    l = list(s)
+    start, end = 0, len(l)-1
+
+    while start < end:
+        while not l[start].isalnum() and start<end:
+            start += 1
+        while not l[end].isalnum() and start<end:
+            end -= 1
+        if l[start].lower() != l[end].lower():
+            return False
+        start, end = start+1, end-1
+    return True
