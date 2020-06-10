5e8820b63c2e1d2e7b3014f4c71391dcd24d29f7
asset311 <32253546+Aseka311@users.noreply.github.com>
2020-06-10 13:51:42 -0400

    minor comments

--- remove_nth_node_from_end_list.py
+++ remove_nth_node_from_end_list.py
@@ -8,17 +8,18 @@ Given linked list: 1->2->3->4->5, and n = 2.
 After removing the second node from the end, the linked list becomes 1->2->3->5.
 '''
 
+# We need to reduce the number of passes we make
 # We use two pointer solution, where first progresses n+1 nodes in front of the second
 # then we just relink the second to the next next node
 # Time complexity is O(n) where n is the lenght of the linked list
-# Space complexity is o(1) since we only used constant extra space
+# Space complexity is O(1) since we only used constant extra space
 
 
 def removeNthFromEnd(head: ListNode, n: int) -> ListNode:
     dummy = ListNode()
     dummy.next = head
 
-    first = dummy.next
+    first = head
     for _ in range(n):      #traverse the first pointer n nodes to the front
         first = first.next
 
