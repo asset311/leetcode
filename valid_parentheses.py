0335f8d1c495e22754b0c67f7a5f30ed32519e99
asset311 <32253546+Aseka311@users.noreply.github.com>
2020-06-10 23:00:41 -0400

    20. Valid Parentheses

---
+++ valid_parentheses.py
@@ -0,0 +1,40 @@
+'''
+20. Valid Parentheses
+Given a string containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+An input string is valid if:
+
+1. Open brackets must be closed by the same type of brackets.
+2. Open brackets must be closed in the correct order.
+Note that an empty string is also considered valid.
+'''
+def isValid(s:str) -> bool:
+    left_chars = []
+
+    lookup = {
+        ')': '(',
+        '}': '{',
+        ']': '['
+    }
+
+    for c in s:
+        if c in lookup:
+            if left_chars.pop() != lookup[c]:
+                return False
+        else:
+            left_chars.append(c)
+        return not left_chars
+    
+def isValid(s:str) -> bool:
+    left_chars = []
+    lookup = {'(':')', '{':'}', '[':']'}
+
+    for c in s:
+        if c in lookup:
+            left_chars.append(c)
+        elif not left_chars or lookup[left_chars.pop()] != c:
+            return False
+
+    return not left_chars 
+
+    # Time complexity is O(n) since for each character we perform O(1) operations
