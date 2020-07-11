58c7b429a44be05d02c392ff543e48d7188d5f80
asset311 <32253546+Aseka311@users.noreply.github.com>
2020-07-11 00:28:37 -0400

    diameter of a binary tree

---
+++ top facebook problems/trees and graphs/diameter_of_binary_tree.py
@@ -0,0 +1,28 @@
+'''
+'''
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def diameterOfBinaryTree(self, root: TreeNode) -> int:
+        
+        diam = 0
+        def depth(root):
+            nonlocal diam
+            
+            if not root:
+                return 0
+            left = depth(root.left)
+            right = depth(root.right)
+            # diameter so far
+            diam = max(diam, left + right)
+            
+            # depth
+            return max(left, right) + 1
+        
+        depth(root)
+        return diam
